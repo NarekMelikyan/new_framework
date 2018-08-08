b641c3d2ca5bb0e94b6a08a8429090ccb89ba9b0
Narekmovsesyan123456 <narmovsesyan123456@gmail.com>
2018-08-08 11:18:12 +0400

    connection singleton

--- application/core/Connection.php
+++ application/core/Connection.php
@@ -11,11 +11,11 @@ namespace application\core;
 
 class Connection
 {
-    private $_connection;
-    private static $_instance; //The single instance
+    private $connection;
+    private static $instance; //The single instance
 
     private function __construct() {
-        $this->_connection = new \PDO('mysql:host=' . constant('DB_HOST') . ';dbname=' . constant('DB_DATABASE'), constant('DB_USERNAME'), constant('DB_PASSWORD'));
+        $this->connection = new \PDO('mysql:host=' . constant('DB_HOST') . ';dbname=' . constant('DB_DATABASE'), constant('DB_USERNAME'), constant('DB_PASSWORD'));
     }
 
     /*
@@ -23,17 +23,17 @@ class Connection
 	@return Instance
 	*/
     public static function getInstance() {
-        if(!self::$_instance) { // If no instance then make one
-            self::$_instance = new self();
+        if(!self::$instance) { // If no instance then make one
+            self::$instance = new self();
         }
-        return self::$_instance;
+        return self::$instance;
     }
 
     // Magic method clone is empty to prevent duplication of connection
     private function __clone() { }
 
     public function getConnection() {
-        return $this->_connection;
+        return $this->connection;
     }
 
 
